aa3a2e2f5f8e4c627574afe131e5d16e741b8080
Calvin Cheng <linchuan.cheng@gmail.com>
2015-05-03 02:18:40 +0800

    Initial Commit

---
+++ BMITime2/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  BMITime2
+//
+//  Created by Calvin Cheng on 3/5/15.
+//  Copyright (c) 2015 Hello HQ Pte. Ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
